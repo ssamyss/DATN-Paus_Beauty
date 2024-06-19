6d4be23ee72fd93990bd1b975c747935d7640e77
phamb <phamb@DESKTOP-ROQ08P5>
2024-06-20 01:48:53 +0700

    //Them form & chuyen sang thymeleaf :v

---
+++ src/main/java/com/poly/controller/AdminController.java
@@ -0,0 +1,48 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	@GetMapping("")
+	public String index() {
+		return "admin/index";
+	}
+
+	@GetMapping("/dataTable")
+	public String dataTable() {
+		return "admin/table-data-table";
+	}
+
+	@GetMapping("/dataProduct")
+	public String dataProduct() {
+		return "admin/table-data-product";
+	}
+
+	@GetMapping("/dataOrder")
+	public String dataOrder() {
+		return "admin/table-data-order";
+	}
+
+	@GetMapping("/baocao")
+	public String baoCao() {
+		return "admin/quan-ly-bao-cao";
+	}
+
+	@GetMapping("/donhang")
+	public String donHang() {
+		return "admin/form-add-don-hang";
+	}
+
+	@GetMapping("/nhanvien")
+	public String nhanVien() {
+		return "admin/form-add-nhan-vien";
+	}
+
+	@GetMapping("/sanpham")
+	public String sanPham() {
+		return "admin/form-add-san-pham";
+	}
+
+}
