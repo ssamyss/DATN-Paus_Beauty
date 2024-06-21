a481b7eb2d8f0e5e5a66747dbb67004aa7c0d663
Admin <Admin@Admin>
2024-06-21 15:33:49 +0700

    // tu dong tang

--- src/main/java/com/poly/entity/DanhMucLoaiSanPham.java
+++ src/main/java/com/poly/entity/DanhMucLoaiSanPham.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -17,6 +19,7 @@ import jakarta.persistence.Table;
 public class DanhMucLoaiSanPham implements Serializable{
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ma_dmlsp")
 	private Integer maLSP;
 	
--- src/main/java/com/poly/entity/LoaiSanPham.java
+++ src/main/java/com/poly/entity/LoaiSanPham.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,6 +21,7 @@ import jakarta.persistence.Table;
 public class LoaiSanPham implements Serializable{
 
 		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		@Column(name = "ma_lsp")
 		private Integer maPL;
 		
