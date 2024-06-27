ad33087a0c8c34805258691359a6538270671a45
hoang <hoang@DESKTOP-MQ30EVP>
2024-06-27 12:58:55 +0700

    //gd user

--- bin/src/main/resources/static/js/sanpham.js
+++ bin/src/main/resources/static/js/sanpham.js
@@ -40,6 +40,60 @@ app.controller("sanpham-ctrl", function($scope, $http) {
         });
     };
 
+
+	$scope.create = function() {
+		var item = angular.copy($scope.form);
+		$http.post('/rest/products', item).then(resp => {
+			$scope.items.push(resp.data);
+			$scope.reset();
+			alert("Thêm mới sản phẩm thành công");
+		}).catch(error => {
+			alert("Lỗi thêm mới!");
+			console.log("Error", error);
+		});
+	};
+
+	$scope.update = function() {
+		var item = angular.copy($scope.form);
+		$http.put('/rest/products/' + item.id, item).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id);
+			$scope.items[index] = item;
+			alert("Cập nhật sản phẩm thành công");
+		}).catch(error => {
+			alert("Lỗi cập nhật!");
+			console.log("Error", error);
+		});
+	};
+	
+	$scope.delete = function(item) {
+		$http.delete('/rest/products/' + item.id).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id);
+			$scope.items.splice(index, 1);
+			$scope.reset();
+			alert("Xóa sản phẩm thành công");
+
+			// Gọi lại hàm để cập nhật dữ liệu từ server
+			$scope.initialize();
+		}).catch(error => {
+			alert("Lỗi xóa dữ liệu!");
+			console.log("Error", error);
+		});
+	};
+	
+	$scope.imageChanged = function(files) {
+		var data = new FormData();
+		data.append('file', files[0]);
+		$http.post('/rest/upload/product', data, {
+			transformRequest: angular.identity,
+			headers: { 'Content-Type': undefined }
+		}).then(resp => {
+			$scope.form.image = resp.data.name;
+		}).catch(error => {
+			alert("Lỗi tải ảnh!");
+			console.log("Error", error);
+		});
+	};
+
     $scope.initialize();
     $scope.reset();
 });
