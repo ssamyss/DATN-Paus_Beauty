d7a6736fecfb510e4d13f713540a41eede1abf53
Admin <Admin@Admin.lan>
2024-06-27 09:46:14 +0700

    // findALL đơn hàng và 1 ít gia lông của chi tiết đơn hàng

--- src/main/resources/static/js/donhang.js
+++ src/main/resources/static/js/donhang.js
@@ -5,6 +5,7 @@ app.controller("donhang-ctrl", function($scope, $http) {
 	};;
 	$scope.accounts = [];
 	$scope.items = [];
+	$scope.byitems = [];
 
 	$scope.reset = function() {
 		$scope.form = {
@@ -23,16 +24,20 @@ app.controller("donhang-ctrl", function($scope, $http) {
 		}).catch(error => {
 			console.error("Lỗi khi tải đơn hàng:", error);
 		});
-		$http.get("/rest/taikhoan").then(resp => {
-			$scope.accounts = resp.data;
-			$scope.items.forEach(item => {
-				item.createDate = new Date(item.createDate);
-			});
-			console.log("Tài khoản:", resp.data);
-		}).catch(error => {
-			console.error("Lỗi khi tải tài khoản:", error);
-		});
 	};
+
+	$scope.show = function(item) {
+		$scope.byitems = angular.copy(item);
+		$http.get('/rest/donhang/' + $scope.byitems.maDH).then(resp => {
+			$scope.items = resp.data;
+			
+			console.log("Đơn hàng show:", resp.data);
+			alert("Thêm mới thương hiệu thành công", resp.data);
+		}).catch(error => {
+			alert("Lỗi thêm mới!");
+			console.log("Error", error);
+		});
+	}
 
 	$scope.initialize();
 	/*$scope.reset();*/
