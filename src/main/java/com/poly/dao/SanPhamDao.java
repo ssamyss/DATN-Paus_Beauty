cbe1f867a58de29ece0a2ae74d1bd0d130f6b988
Tuan Anh <phamb@DESKTOP-DIMOLK5>
2024-07-30 15:53:28 +0700

    //Sua mot xiu :v

--- src/main/java/com/poly/dao/SanPhamDao.java
+++ src/main/java/com/poly/dao/SanPhamDao.java
@@ -1,11 +1,13 @@
 package com.poly.dao;
-import java.util.List;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poly.entity.SanPham;
 import com.poly.entity.ThuongHieu;
 
 public interface SanPhamDao extends JpaRepository<SanPham, Integer> {
-
+	Page<SanPham> findAll(Pageable pageable);
 }
